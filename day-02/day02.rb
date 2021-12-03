43046498ad8dca9c2931f16d741824322aa80024
Aleksey Zolotov <myxrome@gmail.com>
2021-12-03 23:37:09 +0400

    Add the second part of day-02 solutions

--- day-02/day02.rb
+++ day-02/day02.rb
@@ -1,18 +1,18 @@
 # frozen_string_literal: true
 
 def follow_course(input)
-  input.inject([0, 0]) do |coordinate, command|
-    x, d = *coordinate
+  input.inject([0, 0, 0]) do |coordinate, command|
+    x, d, aim = *coordinate
     direction, val = *command.split(' ')
     val = val.to_i
 
     case direction
-    when 'forward' then [x + val, d]
-    when 'up' then [x, d - val]
-    when 'down' then [x, d + val]
-    else [x, d]
+    when 'forward' then [x + val, d + (aim * val), aim]
+    when 'up' then [x, d, aim - val]
+    when 'down' then [x, d, aim + val]
+    else [x, d, aim]
     end
-  end.inject(1, :*)
+  end.take(2).inject(1, :*)
 end
 
 input = File.readlines('input.txt')
